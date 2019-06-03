943c9414050a3791c5338926342a43af732d3b8a
Stanislav Gumeniuk <stanislav.gumeniuk@btsdigital.kz>
2019-06-03 14:16:28 +0300

    Add params for global intercepetrs

--- interceptors.go
+++ interceptors.go
@@ -2,10 +2,14 @@ package jsonrpc
 
 import (
 	"context"
+	"encoding/json"
 )
 
 //InterceptorCallMethod interface for interceptor method
-type InterceptorCallMethod func(ctx context.Context) (context.Context, int, error)
+type InterceptorCallMethod func(ctx context.Context,
+	methodName string,
+	data json.RawMessage,
+	id interface{}) (context.Context, int, error)
 
 //InterceptorCallMethods registry for global interceptors
 type InterceptorCallMethods []InterceptorCallMethod
@@ -15,9 +19,12 @@ func (j *JSONRPC) RegisterGlobalInterceptorCall(method InterceptorCallMethod) {
 	j.globalInterceptors = append(j.globalInterceptors, method)
 }
 
-func (j *JSONRPC) callGlobalInterceptors(ctx context.Context) (context.Context, int, error) {
+func (j *JSONRPC) callGlobalInterceptors(ctx context.Context,
+	methodName string,
+	data json.RawMessage,
+	id interface{}) (context.Context, int, error) {
 	for _, interceptor := range j.globalInterceptors {
-		ctx, code, err := interceptor(ctx)
+		ctx, code, err := interceptor(ctx, methodName, data, id)
 		if err != nil {
 			return ctx, code, err
 		}
